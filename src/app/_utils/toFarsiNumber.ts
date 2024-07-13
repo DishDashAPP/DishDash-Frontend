03a110937ab49e6beba6bdb7d30dc770f0cc72e0
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-07-13 23:34:15 +0330

    feat: add to farsi number utility

---
+++ src/app/_utils/toFarsiNumber.ts
@@ -0,0 +1,12 @@
+export function toFarsiNumber(n: number | undefined) {
+  if (n === undefined)
+      return ''
+
+  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+  return n
+    .toString()
+    .split('')
+    .map(x => farsiDigits[Number(x)])
+    .join('');
+}

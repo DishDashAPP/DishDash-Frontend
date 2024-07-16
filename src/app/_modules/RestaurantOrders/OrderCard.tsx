118d482c3e77a37672de52cf59212f2095f96de3
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-07-16 15:56:12 +0330

    feat: add order details

--- src/app/_modules/RestaurantOrders/OrderCard.tsx
+++ src/app/_modules/RestaurantOrders/OrderCard.tsx
@@ -1,29 +1,33 @@
-'use client'
+'use client';
 
-import {orderItem} from "@utils/types";
-import {FC} from "react";
-import Badge from "@components/Badge/Badge";
-import Button from "@components/Button/Button";
+import { useState } from 'react';
+import { orderItem } from '@utils/types';
+import { FC } from 'react';
+import Badge from '@components/Badge/Badge';
+import Button from '@components/Button/Button';
+import BottomSheet from '@components/BottomSheet/BottomSheet';
+import OrderDetails from "@modules/RestaurantOrders/OrderDetails";
 
-type orderCardProps = {
-    order: orderItem
-}
+type OrderCardProps = {
+    order: orderItem;
+};
 
-const OrderCard: FC<orderCardProps> = ({order}) => {
+const OrderCard: FC<OrderCardProps> = ({ order }) => {
+    const [isBottomSheetOpen, setBottomSheetOpen] = useState(false);
 
     const badgeText = {
         preparing: 'جدید',
         delivering: 'در حال ارسال',
-        delivered: 'تحویل داده شده'
-    }[order.status]
+        delivered: 'تحویل داده شده',
+    }[order.status];
 
-    const isActiveOrder = order.status === 'preparing'
+    const isActiveOrder = order.status === 'preparing';
 
-    return(
+    return (
         <div className="flex flex-col rounded-lg border border-gray-border p-4 w-full">
             <div className="flex items-center justify-between">
                 <span>{order.user.name}</span>
-                <Badge text={badgeText} color={isActiveOrder ? 'green' : 'red'}/>
+                <Badge text={badgeText} color={isActiveOrder ? 'green' : 'red'} />
             </div>
             <div className="flex items-center justify-between my-4">
                 <div>
@@ -32,12 +36,20 @@ const OrderCard: FC<orderCardProps> = ({order}) => {
                 </div>
                 <span>{order.createdAt}</span>
             </div>
-            <Button variant="secondary" className="py-3 tex-base">
+            <Button
+                variant="secondary"
+                className="py-3 text-base"
+                onClick={() => setBottomSheetOpen(true)}
+            >
                 {isActiveOrder ? 'جزئیات سفارش' : 'مشاهده‌ی فاکتور'}
             </Button>
+
+            <BottomSheet open={isBottomSheetOpen} onOpenChange={setBottomSheetOpen}>
+                <h2 className="text-base font-semibold mt-4 mb-6">{isActiveOrder ? 'جزئیات سفارش' : 'فاکتور'}</h2>
+                <OrderDetails orderId={order.id} />
+            </BottomSheet>
         </div>
-    )
-}
+    );
+};
 
 export default OrderCard;
-

1392b82aa373a927eecb3c55639ce50e8599a379
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-07-20 22:12:30 +0330

    Orders.tsx completed

---
+++ src/app/_modules/Customer/ShoppingCarts/Orders/CommentBottomSheet/CommentBottomSheet.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import {FC} from "react";
+import {Controller, useForm} from "react-hook-form";
+import TextInput from "@components/TextInput/TextInput";
+import Button from "@components/Button/Button";
+import {Rating} from "@mui/material";
+
+const CommentBottomSheet: FC = () => {
+    const {
+        control,
+        handleSubmit,
+        formState: {errors, isSubmitting, isValid},
+    } = useForm<Inputs>({
+        mode: 'onChange',
+        defaultValues: {
+            comment: '',
+            restaurantRate: 0,
+            courierRate: 0
+        }
+    });
+
+    const submit: (data: Inputs) => void = async (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div className={"p-6 text-black"}>
+            <div className={"text-base font-medium"}>ثبت نظر و امتیاز</div>
+            <form className={"mt-12"}>
+                <Controller
+                    key={"comment"}
+                    control={control}
+                    name={"comment"}
+                    rules={{
+                        required: 'نظر الزامی است.',
+                    }}
+                    render={({field: {value, onChange, onBlur, name}}) => (
+                        <TextInput
+                            className={""}
+                            type={"text"}
+                            fullWidth
+                            label={"نظر"}
+                            id={"comment"}
+                            error={`${errors["comment"]?.message || ''}`}
+                            isOptional={false}
+                            value={value}
+                            onChange={onChange}
+                            onBlur={onBlur}
+                            name={name}
+                            autoComplete={"off"}
+                        />
+                    )}
+                />
+
+                <div className={"flex justify-between mt-6"}>
+                    <span className={"text-sm"}>به رستوران چه امتیازی می‌دهید؟</span>
+
+                    <Controller
+                        key={"restaurantRate"}
+                        control={control}
+                        name={"restaurantRate"}
+                        rules={{
+                            required: 'امتیاز الزامی است.',
+                        }}
+                        render={({field: {value, onChange, onBlur, name}}) => (
+                            <Rating
+                                dir={"ltr"}
+                                name={"restaurantRate"}
+                                value={value}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                            />
+                        )}
+                    />
+                </div>
+
+                <div className={"flex justify-between mt-6"}>
+                    <span className={"text-sm"}>به پیک چه امتیازی می‌دهید؟</span>
+
+                    <Controller
+                        key={"courierRate"}
+                        control={control}
+                        name={"courierRate"}
+                        rules={{
+                            required: 'امتیاز الزامی است.',
+                        }}
+                        render={({field: {value, onChange, onBlur, name}}) => (
+                            <Rating
+                                dir={"ltr"}
+                                name={"courierRate"}
+                                value={value}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                            />
+                        )}
+                    />
+                </div>
+
+                <Button
+                    className={"mt-8 w-full"}
+                    disabled={!isValid || isSubmitting}
+                    onClick={handleSubmit(submit)}>
+                    ثبت
+                </Button>
+            </form>
+        </div>
+    )
+}
+
+export default CommentBottomSheet;
--- src/app/_modules/Customer/ShoppingCarts/ShoppingCartCard/ShoppingCartCard.tsx
+++ src/app/_modules/Customer/ShoppingCarts/ShoppingCartCard/ShoppingCartCard.tsx
@@ -1,41 +1,77 @@
 'use client'
 
-import {FC} from "react";
+import {FC, useState} from "react";
 import {ShoppingCartType} from "@utils/types";
 import Rate from "@components/Rate/Rate";
 import Image from "next/image";
 import Button from "@components/Button/Button";
 import Link from "next/link";
 import {SHOPPING_CARTS} from "@utils/links";
-import shoppingCarts from "@modules/Customer/ShoppingCarts/ShoppingCarts";
+import COMMENT from "@public/comment.svg";
+import BottomSheet from "@components/BottomSheet/BottomSheet";
+import CommentBottomSheet from "@modules/Customer/ShoppingCarts/Orders/CommentBottomSheet/CommentBottomSheet";
 
-const ShoppingCartCard: FC<{ shoppingCart: ShoppingCartType }> = ({shoppingCart}) => {
+type ShoppingCartCardProps = {
+    shoppingCart: ShoppingCartType;
+    isHistory?: boolean;
+}
+
+const ShoppingCartCard: FC<ShoppingCartCardProps> = ({shoppingCart, isHistory = false}) => {
+    const [isBottomSheetOpen, setIsBottomSheetOpen] = useState<boolean>(false);
+
+    const handleComment = (e) => {
+        e.preventDefault();
+        setIsBottomSheetOpen(true);
+    }
+
     return (
-        <Link href={SHOPPING_CARTS + "/" + shoppingCart.id}>
-            <div className={"px-8 py-4 mt-7 mb-6 border-[1px] border-gray-card_border rounded-lg"}>
-                <div className={"flex"}>
-                    <span className={"text-sm text-black font-medium ml-2"}>{shoppingCart.restaurantName}</span>
-                    <Rate rate={shoppingCart.restaurantRate} variant={"small"}/>
-                </div>
-
-                <div className={"mt-5"}>
-                    {shoppingCart.foods.map(food => (
-                        <div className={"flex mt-1"} key={food.id}>
-                            <Image src={food.imageSrc} width={30} height={30} alt={food.name}/>
-                            <div className={"flex items-center mr-2 font-normal text-xs text-black"}>
-                                <span className={"ml-2"}>({food.count})</span>
-                                <span>{food.name}</span>
-                            </div>
+        <>
+            <Link href={SHOPPING_CARTS + "/" + shoppingCart.id}>
+                <div className={"px-8 py-4 mt-7 mb-6 border-[1px] border-gray-card_border rounded-lg"}>
+                    <div className={"flex justify-between"}>
+                        <div className={"flex"}>
+                            <span className={"text-sm text-black font-medium ml-2"}>{shoppingCart.restaurantName}</span>
+                            <Rate rate={shoppingCart.restaurantRate} variant={"small"}/>
                         </div>
-                    ))}
-                </div>
+                        {isHistory &&
+                          <div className={"flex"} onClick={handleComment}>
+                            <Image src={COMMENT} width={20} height={20} alt={"comment"} className={"ml-2"}/>
+                            <span className={"text-xs font-normal text-gray-primary underline underline-offset-4"}>
+                          ثبت امتیاز و نظر
+                        </span>
+                          </div>
+                        }
+                    </div>
 
-                <div className={"flex justify-between mt-5 gap-x-4"}>
-                    <Button label={"ادامه‌ی خرید"} className={"w-full"}/>
-                    <Button label={"حذف سبد"} variant={"secondary"} className={"w-full"}/>
+                    <div className={"mt-5"}>
+                        {shoppingCart.foods.map(food => (
+                            <div className={"flex mt-1"} key={food.id}>
+                                <Image src={food.imageSrc} width={30} height={30} alt={food.name}/>
+                                <div className={"flex items-center mr-2 font-normal text-xs text-black"}>
+                                    <span className={"ml-2"}>({food.count})</span>
+                                    <span>{food.name}</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className={"flex justify-between mt-5 gap-x-4"}>
+                        {isHistory ?
+                            <Button label={"مشاهده‌ی فاکتور"} variant={"secondary"} className={"w-full"}/>
+                            :
+                            <>
+                                <Button label={"ادامه‌ی خرید"} className={"w-full"}/>
+                                <Button label={"حذف سبد"} variant={"secondary"} className={"w-full"}/>
+                            </>
+                        }
+                    </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+
+            <BottomSheet open={isBottomSheetOpen} onOpenChange={setIsBottomSheetOpen}>
+                <CommentBottomSheet/>
+            </BottomSheet>
+        </>
     )
 }
 

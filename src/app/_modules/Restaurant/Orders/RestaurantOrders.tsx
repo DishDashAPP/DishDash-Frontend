80c1a976177400020cc1942f6dc8949d63fb371a
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-08-02 12:34:57 +0330

    chore: fix build issues

--- src/app/_modules/Restaurant/Orders/RestaurantOrders.tsx
+++ src/app/_modules/Restaurant/Orders/RestaurantOrders.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import TabSwitch from "@modules/Restaurant/Orders/TabSwitch";
 import OrderList from "@modules/Restaurant/Orders/OrderList";
 
 const RestaurantOrders: FC = () => {
   return (
     <div className="flex flex-col items-center w-full">
-      <TabSwitch />
-      <OrderList />
+      <Suspense fallback={<></>}>
+        <TabSwitch />
+        <OrderList />
+      </Suspense>
     </div>
   );
 };

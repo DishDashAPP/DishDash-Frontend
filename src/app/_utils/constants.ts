7fc2289019049d188e58b32944b3df603cdf59b7
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2024-08-02 13:59:31 +0330

    feat: add date helper function

---
+++ src/app/_utils/constants.ts
@@ -0,0 +1,18 @@
+export enum Num {
+    ZERO = 0,
+    HUNDRED = 100,
+    THOUSAND = 1000,
+    MILLION = 1000000,
+    BILLION = 1000000000,
+}
+
+export enum Times {
+    MILLISECOND = 1,
+    SECOND = 1000,
+    MINUTE = 60000,
+    HOUR = 3600000,
+    DAY = 86400000,
+    WEEK = 604800000,
+    MONTH = 2592000000,
+    YEAR = 31536000000,
+}
